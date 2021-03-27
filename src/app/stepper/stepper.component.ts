40ed6cfc63e3949454c5a5956e75d5c00805a6e7
Anderson Miranda <me@andersonmiranda.com>
2021-03-27 16:47:42 +0100

    stepper

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
+  styleUrls: ['./stepper.component.scss'],
 })
 export class StepperComponent implements OnInit {
+  @Input() step: number;
+  steps: Array<number> = [1, 2, 3];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
